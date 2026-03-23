fc5648870ab89527fc6bdf5dafbb4434c18ad008
Abhi260607 <sharmaabhi22250@gmail.com>
2026-03-23 21:13:46 +0530

    Created a new file of multi function

---
+++ multi_function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int multi(int a, int b)
+{
+    int multiplication;
+    multiplication = (a * b);
+    return multiplication;
+}
+
+int main()
+{
+    cout << multi(6, 9);
+    return 0;
+}
